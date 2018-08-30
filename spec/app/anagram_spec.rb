dfa2122453fb1342cb6e9f9256e0d22b2d42bc8a
tomoya miyahara <t-miyahara@tes.co.jp>
2018-08-30 17:16:45 +0900

    rspec初期配置

---
+++ spec/app/anagram_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require './app/anagram'
+
+RSpec.describe Anagram do
+  pending "add some examples to #{__FILE__}"
+end
---
+++ spec/app/fizz_buzz_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require './app/fizz_buzz'
+
+RSpec.describe FizzBuzz do
+  pending "add some examples to #{__FILE__}"
+end
