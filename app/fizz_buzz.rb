832d688adaf44dc4b5598121372045476de14e6d
tomoya miyahara <t-miyahara@tes.co.jp>
2018-09-04 11:05:11 +0900

    fizzbuzzの回答例作成

--- app/fizz_buzz.rb
+++ app/fizz_buzz.rb
@@ -1,5 +1,30 @@
 class FizzBuzz
+  FIZZ = 'Fizz'.freeze
+  BUZZ = 'Buzz'.freeze
+  FIZZ_BUZZ = (FIZZ + BUZZ).freeze
+  
   def execute
+    (1..100).each do |number|
+      p response(number)
+    end
+  end
+
+  def response(number)
+    raise ArgumentError unless 1 <= number && number <= 100
+
+    if number % 3 == 0 && number % 5 == 0
+      return FIZZ_BUZZ
+    end
+
+    if number % 3 == 0
+      return FIZZ
+    end
+
+    if number % 5 == 0
+      return BUZZ
+    end
+
+    number
   end
 end
 
--- spec/app/fizz_buzz_spec.rb
+++ spec/app/fizz_buzz_spec.rb
@@ -2,5 +2,75 @@ require 'spec_helper'
 require './app/fizz_buzz'
 
 RSpec.describe FizzBuzz do
-  pending "add some examples to #{__FILE__}"
+  let(:instannce) { described_class.new }
+
+  describe '#response' do
+    subject { instannce.response(argument) }
+
+    context '引数が3の倍数である場合' do
+      let(:argument) { 3 }
+
+      it 'Fizzを返すこと' do
+        expect(subject).to eq('Fizz')
+      end
+    end
+
+    context '引数が5の倍数である場合' do
+      let(:argument) { 5 }
+
+      it 'Buzzを返すこと' do
+        expect(subject).to eq('Buzz')
+      end
+    end
+
+    context '引数が3と5の倍数である場合' do
+      let(:argument) { 15 }
+
+      it 'FizzBuzzを返すこと' do
+        expect(subject).to eq('FizzBuzz')
+      end
+    end
+
+    context '引数に1を与えた場合' do
+      let(:argument) { 1 }
+
+      it '1を返すこと' do
+        expect(subject).to eq(1)
+      end
+    end
+
+    context '引数に99を与えた場合' do
+      let(:argument) { 99 }
+
+      it 'Fizzを返すこと' do
+        expect(subject).to eq('Fizz')
+      end
+    end
+
+    context '引数に100を与えた場合' do
+      let(:argument) { 100 }
+
+      it 'Buzzを返すこと' do
+        expect(subject).to eq('Buzz')
+      end
+    end
+    
+    context '無効な値を引数に指定するテスト' do
+      context '引数に0を与えた場合' do
+        let(:argument) { 0 }
+
+        it 'ArgumentErrorが発生すること' do
+          expect {subject}.to raise_error(ArgumentError)
+        end
+      end
+
+      context '引数に101を与えた場合' do
+        let(:argument) { 101 }
+
+        it 'ArgumentErrorが発生すること' do
+          expect {subject}.to raise_error(ArgumentError)
+        end
+      end
+    end
+  end
 end
